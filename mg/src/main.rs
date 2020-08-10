752dc2b2f58e914810e6e2fa8f30b96da7d7fb53
Kiëd Llaentenn <kiedtl@tilde.team>
2020-08-10 13:40:20 +0000

    main: update

--- mg/src/main.rs
+++ mg/src/main.rs
@@ -9,6 +9,7 @@ mod rect;
 use crate::dun_s1::*;
 use crate::drunk::*;
 use crate::cellular::*;
+use crate::randrm::*;
 use rand::prelude::*;
 
 fn main() {
@@ -27,8 +28,8 @@ fn main() {
         .schedule_job(JobType::Generation(false))
         .schedule_job(JobType::Generation(false))
         .schedule_job(JobType::Generation(false))
-        .schedule_job(JobType::Generation(false))
-        .do_work();
+        .schedule_job(JobType::Generation(false));
+        //.do_work();
 
     Drunkard::new(&mut map, &mut rng)
         .center_weight(0.1)
@@ -36,6 +37,14 @@ fn main() {
         .max_iterations(5000)
         .filled_goal(0.25);
         //.walk();
+
+    RandomRooms::new(&mut map, &mut rng)
+        .max_rooms(30)
+        .room_max_width(16)
+        .room_max_height(8)
+        .room_min_width(4)
+        .room_min_height(2)
+        .tunnel();
 
     display(&map);
 }
