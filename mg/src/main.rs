2f7acc6e9e04793e3131f512161ba5eeecb67db3
Kiëd Llaentenn <kiedtl@tilde.team>
2020-08-16 20:36:16 +0000

    initial implementation of noise

--- mg/src/main.rs
+++ mg/src/main.rs
@@ -16,6 +16,8 @@ use crate::randrm::*;
 use serde::Deserialize;
 use std::fs::File;
 use ron::de::from_reader;
+use noise::{Abs, Perlin, OpenSimplex, NoiseFn, Seedable, BasicMulti, Billow, Fbm, HybridMulti, RidgedMulti, Checkerboard, Constant, Cylinders, SuperSimplex, Value, Worley};
+use rand::prelude::*;
 
 #[derive(Debug, Deserialize)]
 enum MapgenAlgorithm {
@@ -40,6 +42,35 @@ struct DungeonSpecification {
 fn main() {
     let mut rng = rand::thread_rng();
     let mut dungeons_s1: Vec<DungeonS1> = Vec::new();
+
+    // OpenSimplex/BasicMulti/Fbm/HybridMulti
+    // OpenSimplex
+    // Cylinders for morgoth's lair
+    // Value -- chunks/islands of high-value minerals
+    let os = Perlin::new().set_seed(rng.gen());
+    let mut map = DungeonS1::new(638, 153); //250, 64);
+    for y in 0..(map.height - 1) {
+        for x in 0..(map.width - 1) {
+            //map.d[y][x] = 
+            let nx: f64 = (x as f64) / (map.width as f64)  - 0.5;
+            let ny: f64 = (y as f64) / (map.height as f64) - 0.5;
+
+            let mut noise =   1.0 * os.get([ 1.0 * ny,  1.0 * nx])
+                          +   0.5 * os.get([ 2.0 * ny,  2.0 * nx])
+                          +  0.25 * os.get([ 4.0 * ny,  4.0 * nx])
+                          + 0.125 * os.get([ 8.0 * ny,  8.0 * nx])
+                          + 0.062 * os.get([16.0 * ny, 16.0 * nx])
+                          + 0.031 * os.get([32.0 * ny, 32.0 * nx]);
+            let color: u8 = (noise.abs().powf(4.12) * 255.0) as u8;
+
+            //println!("{},{}", x, color);
+            print!("{}[48;2;{};{};{}m {}[m", 0x1b as char, color, color, color, 0x1b as char);
+        }
+
+        println!("");
+    }
+
+    return; // DEBUG
 
     let args = std::env::args().collect::<Vec<String>>();
     if args.len() < 2 {
