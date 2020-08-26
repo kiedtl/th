cfbc1afab0b35df46e9895978bbb970e8385efb2
Kiëd Llaentenn <kiedtl@tilde.team>
2020-08-26 01:09:31 +0000

    mv: initial implementation

--- mv/src/main.rs
+++ mv/src/main.rs
@@ -1,4 +1,3 @@
-use pancurses::*;
 use std::{fs, fs::File};
 use ron::de::from_reader;
 use lib::dungeon::*;
@@ -7,16 +6,38 @@ use lib::material::*;
 use lib::id::*;
 use lib::colors::*;
 use lib::dun_s1::*;
+use lib::dun_s2::*;
+use lib::math::*;
 use std::error::Error;
 use std::collections::HashMap;
+use termbox_sys::*;
+
+const NIL_RAW_EVENT: RawEvent = RawEvent { etype: 0, emod: 0, key: 0, ch: 0, w: 0, h: 0, x: 0, y: 0 };
 
 fn main() {
-    let window = initscr();
-    start_color();
-    use_default_colors();
-    cbreak();
-    noecho();
-    window.refresh();
+    // set a custom panic handler that calls tb_shutdown
+    // before printing anything
+    std::panic::set_hook(Box::new(|panic_info| {
+        unsafe { tb_shutdown(); }
+        println!("aborting due to fatal error (see below):");
+        println!("{}", panic_info);
+        println!("stack backtrace:");
+        println!("{:?}", backtrace::Backtrace::new());
+        println!("please report this issue upstream at {}.",
+            "https://github.com/kiedtl/th");
+    }));
+
+    let screen_width;
+    let screen_height;
+
+    unsafe {
+        tb_init();
+        tb_select_output_mode(TB_OUTPUT_TRUECOLOR);
+        tb_set_clear_attributes(TB_WHITE, TB_BLACK);
+        tb_clear();
+        screen_width = tb_width();
+        screen_height = tb_height();
+    }
 
     // check arguments
     let args = std::env::args().collect::<Vec<String>>();
@@ -74,75 +95,241 @@ dyn Error>>
     let mut materials: HashMap<String, MaterialInfo> = HashMap::new();
     load_info_files(&args[0], "../dat/mats/", &mut materials).unwrap();
 
+    let mut cursor_x = 0;
+    let mut cursor_y = 0;
+    let mut current_level = 0;
 
-    let mapwin = newwin(
-        window.get_max_y(),
-        window.get_max_x() - 20,
-        0, 0
-    );
-    mapwin.refresh();
-    draw_map(mapwin, map, &materials, 20, 20, 0);
+    draw_map(&map, screen_width / 2, screen_height - 5,
+        &materials, cursor_x, cursor_y, current_level);
+    draw_desc(&map, screen_width, screen_height,
+        &materials, cursor_x, cursor_y, current_level);
 
-    window.getch();
-    endwin();
+    // write keybindings
+    let kbds: Vec<(&str, &str)> = vec![
+        ("<>",   "change level"),
+        ("hjkl", "move cursor west/south/north/east"),
+        ("HJKL", "like hjkl, but move 8 tiles at a time"),
+        ("q",    "quit"),
+    ];
+
+    for kbd_i in 0..(kbds.len()) {
+        let kbd = kbds[kbd_i];
+        let row = screen_height - 5 + (kbd_i as i32);
+        let mut col: i32 = 0;
+
+        col = tb_put_string(screen_width / 2, screen_height, col, row,
+            kbd.0, 0x000000, 0xffffff, false).1;
+        if col < 5 { col += 5 - col; }
+        tb_put_string(screen_width / 2, screen_height, col, row,
+            kbd.1, 0xffffff, 0x000000, false).1;
+    }
+
+    unsafe { tb_present(); }
+
+    loop {
+        let mut ev = NIL_RAW_EVENT;
+        let t = unsafe { tb_poll_event(&mut ev) };
+
+        if t == -1 { // TODO: check -2, -3
+            unsafe { tb_shutdown(); }
+            panic!("fatal termbox error");
+        }
+
+        if t == (TB_EVENT_KEY as i32) {
+            match std::char::from_u32(ev.ch).unwrap() {
+                'q' => break,
+                'h' => cursor_x = cursor_x.saturating_sub(1),
+                'j' => cursor_y = clamp(cursor_y + 1, 0, map.levels[current_level].height - 1),
+                'k' => cursor_y = cursor_y.saturating_sub(1),
+                'l' => cursor_x = clamp(cursor_x + 1, 0, map.levels[current_level].width - 1),
+                'H' => cursor_x = cursor_x.saturating_sub(8),
+                'J' => cursor_y = clamp(cursor_y + 8, 0, map.levels[current_level].height - 1),
+                'K' => cursor_y = cursor_y.saturating_sub(8),
+                'L' => cursor_x = clamp(cursor_x + 8, 0, map.levels[current_level].width - 1),
+                '>' => current_level = clamp(current_level + 1, 0, map.levels.len() - 1),
+                '<' => current_level = current_level.saturating_sub(1),
+                _ => (),
+            }
+
+            draw_map(&map, screen_width / 2, screen_height - 5,
+                &materials, cursor_x, cursor_y, current_level);
+            draw_desc(&map, screen_width, screen_height,
+                &materials, cursor_x, cursor_y, current_level);
+            unsafe { tb_present(); }
+        }
+
+        // TODO: handle TB_EVENT_RESIZE
+        // TODO: handle TB_EVENT_MOUSE
+    }
+
+    unsafe { tb_shutdown(); }
+}
+
+pub fn draw_desc(
+    map: &Dungeon, max_x: i32, max_y: i32,
+    materials: &HashMap<String, MaterialInfo>,
+    cur_x: usize, cur_y: usize, lvl: usize,
+) {
+    let startx = (max_x / 2 + 1) as i32;
+    let mut row: i32 = 0i32;
+    let mut col: i32 = startx;
+
+    // draw tile
+    let tile = &map.levels[lvl].d[cur_y][cur_x];
+    let cell = tile_as_cell(tile, materials);
+    unsafe { tb_put_cell(col, row, &cell); }
+    col += 2;
+
+    // draw coordinates
+    let dim_str = format!("(row {}/{}, col {}/{}, lvl {}/{})",
+        cur_y + 1, &map.levels[lvl].height, cur_x + 1, &map.levels[lvl].width,
+        lvl + 1, &map.levels.len());
+    tb_put_string(max_x, max_y, col, row, &dim_str, 0xffffff, 0x000000, false);
+    row += 2;
+    col = startx;
+
+    // draw name of material
+    let material = &materials[&tile.tile_material];
+    tb_put_string(max_x, max_y, col, row, &material.name,
+        0xffffff, 0x000000, false);
 }
 
 pub fn draw_map(
-    win: Window, map: Dungeon,
+    map: &Dungeon, max_x: i32, max_y: i32,
     materials: &HashMap<String, MaterialInfo>,
     cur_x: usize, cur_y: usize, lvl: usize
 ) {
     let level  = &map.levels[lvl];
-    let starty = cur_y.saturating_sub((win.get_max_y() as usize) / 2);
-    let endy   = cur_y + ((win.get_max_y() as usize) / 2);
-    let startx = cur_x.saturating_sub((win.get_max_x() as usize) / 2);
-    let endx   = cur_x + ((win.get_max_x() as usize) / 2);
+
+    let starty = (cur_y as isize) - ((max_y as isize) / 2);
+    let endy   = (cur_y + ((max_y as usize) / 2)) as isize;
+    let startx = (cur_x as isize) - ((max_x as isize) / 2);
+    let endx   = (cur_x + ((max_x as usize) / 2)) as isize;
+
+    // screen position
+    let mut yctr = 0usize;
+    let mut xctr = 0usize;
 
     for y in starty..endy {
         for x in startx..endx {
-            let tile_material = &materials[&level.d[y][x].tile_material];
-            let mut bg = tile_material.color_bg;
-            let mut fg = tile_material.color_fg;
-            let mut glyph: char;
+            // if out of bounds of the map, just draw a black tile
+            if (y < 0 || x < 0) ||
+                (y as usize >= level.height || x  as usize >= level.width) {
+                    unsafe {
+                        tb_change_cell(xctr as i32, yctr as i32,
+                            ' ' as u32, 0xffffff, 0x000000);
+                    }
 
-            match level.d[y][x].tiletype {
-                TileType::Debug
-                | TileType::Wall  => {
-                    glyph = tile_material.block_glyph;
-                },
-                TileType::Floor => {
-                    glyph = '+';
-                    bg = Color::new(0, 0, 0, 0);
-                },
+                    if xctr >= max_x as usize { break; }
+                    else { xctr += 1; }
+                    continue;
             }
 
-            if level.d[y][x].mobs.len() > 0 {
-                let mob = &level.d[y][x].mobs[0];
-                bg = Color::new(0, 0, 0, 0);
-                glyph = mob.unicode_glyph;
-                if let Some(mob_fg) = mob.glyph_fg {
-                    fg = mob_fg;
-                } else {
-                    fg = Color::new(0, 0, 0, 0);
-                }
+            let mut cell = tile_as_cell(&level.d[y as usize][x as usize],
+                materials);
+            if x as usize == cur_x && y as usize == cur_y {
+                cell.bg = Color::new(200, 200, 0, 0).as_u32();
             }
+            unsafe { tb_put_cell(xctr as i32, yctr as i32, &cell); }
 
-            let fg_256 = (((fg.red as f64)*6.0/256.0)*36.0
-                + ((fg.green as f64)*6.0/256.0)*6.0 + ((fg.blue as f64)*6.0/256.0)) as i16;
-            let bg_256 = (((bg.red as f64)*6.0/256.0)*36.0
-                + ((bg.green as f64)*6.0/256.0)*6.0 + ((bg.blue as f64)*6.0/256.0)) as i16;
-            init_pair(x as i16, fg_256, bg_256);
-            //init_pair(2, 156, -1);
-
-            //win.printw(format!("{}[38;2;{};{};{}m{}[48;2;{};{};{}m{}{}[m",
-                //0x1b as char, fg.red, fg.green, fg.blue,
-                //0x1b as char, bg.red, bg.green, bg.blue,
-                //glyph, 0x1b as char));
-            win.attrset(ColorPair(x as u8));
-            win.printw(format!("{}", glyph));
+            if xctr >= max_x as usize { break; }
+            else { xctr += 1; }
         }
-        win.printw("\n");
+
+        if yctr >= max_y as usize { break; }
+        else { yctr += 1; }
+
+        xctr = 0;
+    }
+}
+
+fn tile_as_cell(tile: &DungeonTile, materials: &HashMap<String, MaterialInfo>) -> RawCell {
+    let tile_material = &materials[&tile.tile_material];
+    let mut bg = tile_material.color_bg;
+    let mut fg = tile_material.color_fg;
+    let mut glyph: char;
+
+    match tile.tiletype {
+        TileType::Debug
+        | TileType::Wall  => {
+            glyph = tile_material.block_glyph;
+        },
+        TileType::Floor => {
+            glyph = '+';
+            bg = Color::new(0, 0, 0, 0);
+        },
     }
 
-    win.refresh();
+    if tile.mobs.len() > 0 {
+        let mob = &tile.mobs[0];
+        bg = Color::new(0, 0, 0, 0);
+        glyph = mob.unicode_glyph;
+        if let Some(mob_fg) = mob.glyph_fg {
+            fg = mob_fg;
+        } else {
+            fg = Color::new(0, 0, 0, 0);
+        }
+    }
+
+    RawCell {
+        ch: glyph as u32,
+        fg: fg.as_u32(),
+        bg: bg.as_u32(),
+    }
+}
+
+fn tb_put_string(
+    max_x: i32, max_y: i32,
+    col: i32, row: i32,
+    str: &str,
+    fg: u32, bg: u32,
+    wrap: bool
+) -> (i32, i32) {
+    let mut ccol = col;
+    let mut crow = row;
+    for c in str.chars() {
+        unsafe {
+            tb_put_cell(ccol, crow, &RawCell {
+                ch: c as u32,
+                fg: fg, bg: bg
+            });
+        }
+
+        if (ccol + 1) == (max_x - 1) {
+            if wrap && crow + 1 != max_y {
+                crow += 1;
+                ccol = col;
+            } else {
+                let dot = RawCell {
+                    ch: '.' as u32,
+                    fg: fg, bg: bg,
+                };
+
+                // draw some nice ellipses
+                unsafe {
+                    tb_put_cell(ccol - 2, crow, &dot);
+                    tb_put_cell(ccol - 1, crow, &dot);
+                    tb_put_cell(ccol - 0, crow, &dot);
+                }
+
+                break;
+            }
+        } else {
+            ccol += 1;
+        }
+    }
+
+    // clear to the end of the line
+    let clear_cell = RawCell {
+        ch: ' ' as u32,
+        fg: 0xffffff, bg: 0x000000,
+    };
+
+    for ncol in ccol..max_x {
+        unsafe {
+            tb_put_cell(ncol, row, &clear_cell);
+        }
+    }
+
+    (crow, ccol)
 }
