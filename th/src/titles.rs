c37173416f34c3b5d89f5077740d532fc7bf3b37
Kiëd Llaentenn <kiedtl@tilde.team>
2020-08-28 00:46:24 +0000

    src/titles.rs: add lol

---
+++ th/src/titles.rs
@@ -0,0 +1,35 @@
+// TODO
+const PLAYER_TITLES: &[&str] = [
+    "Orc Fodder",
+    "Greenhorn",
+    "Unwelcome Visitor",
+    "Sneak",
+    "Legend",
+];
+
+const CONFUSED_PLAYER_TITLES: &[&str] = [
+    // should be obvious what this is
+    "Befuddled",
+    "Drunk",
+    "Clueless",
+
+    // delusions of grandeur
+    "Demon Butcher",
+    "Bane of Melkor",
+    "King of Middle Earth",
+    "Grue-chomper",
+
+    // misc lol
+    "President",
+    "Poisonous beast",
+    "Kim Jong Un (undercover)",
+    "Queen of England",
+    "Emperor of Japan",
+
+    // player imagines themself to be
+    // melkor or sauron
+    "Lord of Angband",
+    "Dark Enemy",
+    "Lord of Abominations",
+    "Necromancer",
+];
